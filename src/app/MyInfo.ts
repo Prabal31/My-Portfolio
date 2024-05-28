34b0f0613d8883d92a18a3897a4f695b8ecffe30
Prabal <manchandaprabal878@gmail.com>
2024-05-28 12:00:31 -0400

    added one class and obejct of my info

---
+++ src/app/MyInfo.ts
@@ -0,0 +1,1 @@
+export class Personal { first!: string; last!: string;}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Personal} from "./MyInfo";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  mydata:Personal = {first:"Prabal",last:"Manchanda"};
 }
