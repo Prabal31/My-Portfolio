e880e69f11278b287432b75b623e8db4cb5b8d47
Prabal <manchandaprabal878@gmail.com>
2024-06-02 16:00:58 -0400

    made some changes in the nav bar on the top added some animantion and now when the tab is clicked the section of the html changes ealier the section was changing when clicked on the text only now it changes when clicked anywhere.

--- src/app/Menu/menu.component.ts
+++ src/app/Menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs'
 
 
 @Component({
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+
+  tabChanged(event: MatTabChangeEvent) {
+    const selectedIndex = event.index;
+
+    // Based on the selected index, you can perform actions such as changing the section
+    const sectionId = ["home", "about", "work", "projects", "contact"][selectedIndex];
+    const element = document.getElementById(sectionId);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
   
 
 }
