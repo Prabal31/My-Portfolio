fda33aa46c0e407c939e494d361ffcc89b5ca166
Prabal <manchandaprabal878@gmail.com>
2024-06-10 10:17:59 -0400

    Added my photo in json so i can use it on multiply components and reduce typing

--- src/app/MyInfo.ts
+++ src/app/MyInfo.ts
@@ -1,4 +1,4 @@
-export class Personal { first!: string; last!: string;}
+export class Personal { first!: string; last!: string; image!: string}
 export class Links {Linkedin!: string; GitHub!: String; Email!: string}
 export class Project {
     name!: string;
