12721cc76a97c57868307917ad3dc1b05c46a5b8
Prabal <manchandaprabal878@gmail.com>
2024-06-11 12:50:19 -0400

    Made some changes in json data and removed an array from project technology section

--- src/app/MyInfo.ts
+++ src/app/MyInfo.ts
@@ -1,9 +1,9 @@
-export class Personal { first!: string; last!: string; image!: string}
+export class Personal { first!: string; last!: string; image!: string; about!: string}
 export class Links {Linkedin!: string; GitHub!: String; Email!: string}
 export class Project {
     name!: string;
     image!: string;
-    technology_used!: string[];
+    technology_used!: string;
     details!: string;
     github_link!: string;
 }
