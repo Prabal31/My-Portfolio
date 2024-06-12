4737e5b2f6fd5189a4c31fb8701524c4f9e6891a
Prabal <manchandaprabal878@gmail.com>
2024-06-11 22:56:02 -0400

    Chnaged Class to interface for more flexliblity

--- src/app/MyInfo.ts
+++ src/app/MyInfo.ts
@@ -1,32 +1,32 @@
-export class Personal { 
-    first!: string; 
-    last!: string; 
-    image!: string;
-    occupation!: string;
-    discription!: string
-    about!: string
+export interface Personal { 
+    first: string; 
+    last: string; 
+    image: string;
+    occupation: string;
+    discription: string
+    about: string
 }
-export class Links {
-    Linkedin!: string; 
-    GitHub!: String; 
-    Email!: string
+export interface Links {
+    Linkedin: string; 
+    GitHub: String; 
+    Email: string
 }
-export class Project {
-    name!: string;
-    image!: string;
-    technology_used!: string;
-    details!: string;
-    github_link!: string;
+export interface Project {
+    name: string;
+    image: string;
+    technology_used: string;
+    details: string;
+    github_link: string;
 }
-export class Experience{
-    Company_name!: string; 
-    Position!: string; 
-    Time_frame!: string;
-    Company_logo!: string;
-    Company_link!: string;
-    Point1!: string;
-    Point2!: string;
-    Point3!: string;
-    Point4!: string;
-    Point5!: string;
+export interface Experience{
+    Company_name: string; 
+    Position: string; 
+    Time_frame: string;
+    Company_logo: string;
+    Company_link: string;
+    Point1: string;
+    Point2: string;
+    Point3: string;
+    Point4: string;
+    Point5: string;
 }
